02afe5df1de423ccccbd7101de3549872463d73d
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-17 18:29:10 -0700

    added manager test

---
+++ tests/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require("../lib/Manager");
+
+test("Return office number to equal the tested value", () => {
+  const testValue = officeNumber;
+  const employee = new Manager("Dustin", 1, "test@email.com", testValue);
+  expect(employee.officeNumber).toBe(testValue);
+});
+
+test("Manager should be returned from getRole", () => {
+  const testValue = "Manager";
+  const employee = new Manager("Dustin", 1, "test@email.com");
+  expect(employee.getRole()).toBe(testValue);
+});
