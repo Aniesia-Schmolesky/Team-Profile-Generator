61e0181d1f83999f88a8d62d37032fc999daee94
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-10-17 17:53:25 -0700

    added beginning of html

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const fs = require("fs");
 const team = [];
 
 function startFile() {
+    beginHtml();
     newTeamMember();
 }
 
@@ -66,7 +67,7 @@ function newTeamMember() {
             } else {
                 additionalMember = new Intern(name, id, email, positionSpecifics);
             }
-            employees.push(additionalMember);
+            team.push(additionalMember);
             addHtml(additionalMember)
             .then(function() {
                 if (additionalMembers === "yes") {
@@ -78,4 +79,28 @@ function newTeamMember() {
             
         });
     });
+}
+
+function beginHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>My Team</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("begin");
 }
